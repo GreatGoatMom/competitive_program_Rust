7b9d1b5fa64d0bb10e267da43cb947cf5246c36c
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-22 19:03:38 +0900

    1

--- abc178/src/bin/a.rs
+++ abc178/src/bin/a.rs
@@ -1,3 +1,15 @@
+use proconio::*;
+
+#[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        n : u32,
+    }
+    let ans;
+    if n == 1 {
+        ans = 0;
+    } else {
+        ans = 1;
+    }
+    println!{"{}", ans};
 }
