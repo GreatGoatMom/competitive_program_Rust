c5557d1857ff167eacfb6ddfd50856f8399ba9b3
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-22 21:38:51 +0900

    make abc175

---
+++ target/debug/deps/a-07584f4afdc406aa.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/a-07584f4afdc406aa: src/bin/a.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/a-07584f4afdc406aa.d: src/bin/a.rs
+
+src/bin/a.rs:
---
+++ target/release/deps/a-c3ea05dcdb381765.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/release/deps/a-c3ea05dcdb381765: src/bin/a.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/release/deps/a-c3ea05dcdb381765.d: src/bin/a.rs
+
+src/bin/a.rs:
