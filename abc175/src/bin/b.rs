9d3587ac1a66dbc39933a570586fc206c086d5cc
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-22 23:16:51 +0900

    11

--- abc175/src/bin/b.rs
+++ abc175/src/bin/b.rs
@@ -5,5 +5,29 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        n : usize,
+        num_list : [i64; n]    
+    }
+    let mut ans = 0;
+    if n < 3 {
+        println!("{}",0);
+        return;
+    }
+    for a in 0..(n-2) {
+        for b in (a+1)..n-1 {
+            for c in (b+1)..n {
+                if (num_list[a] == num_list[b] || num_list[b] == num_list[c] || num_list[c] == num_list[a]) {
+                    continue;
+                }
+                let x = num_list[a] + num_list[b];
+                let y = num_list[b] + num_list[c];
+                let z = num_list[c] + num_list[a];
+                if x > num_list[c] && y > num_list[a] && z > num_list[b] {
+                    ans += 1;
+                }
+            }
+        }
+    }
+    println!("{}",ans);
 }
---
+++ target/debug/deps/b-556511c406219b2f.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-556511c406219b2f: src/bin/b.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-556511c406219b2f.d: src/bin/b.rs
+
+src/bin/b.rs:
