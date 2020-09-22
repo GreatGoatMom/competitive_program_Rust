daf3f23e013aa160f8b96e4e75774a17f184c517
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-22 22:36:19 +0900

    10

--- abc175/src/bin/a.rs
+++ abc175/src/bin/a.rs
@@ -1,9 +1,24 @@
 use proconio::*;
+use proconio::marker::{Bytes, Chars};
 use std::*;
 
 const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        s : Chars
+    }
+    let mut ans = 0;
+    let mut tmp = 0;
+    for ind in 0..s.len() {
+        if s[ind] == 'R'{
+            tmp+=1;
+        } else {
+            ans = cmp::max(ans,tmp);
+            tmp = 0;
+        }
+    }
+    ans = cmp::max(ans,tmp);
+    println!("{}",ans);
 }
--- target/debug/a.d
+++ target/debug/a.d
@@ -1,1 +1,1 @@
-/mnt/c/Users/81906/competitive_program_Rust/target/debug/a: /mnt/c/Users/81906/competitive_program_Rust/abc176/src/bin/a.rs build.rs
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/a: /mnt/c/Users/81906/competitive_program_Rust/abc175/src/bin/a.rs build.rs
