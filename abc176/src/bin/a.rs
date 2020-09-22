7b216b34863717fcfa191d32f4960bfad0267ba4
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-22 22:30:52 +0900

    9

--- abc176/src/bin/a.rs
+++ abc176/src/bin/a.rs
@@ -5,5 +5,15 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        n :i32,
+        x :i32,
+        t :i32,
+    }
+    let mut divCnt = n / x;
+    if n % x != 0 {
+        divCnt += 1;
+    }
+    let ans = divCnt * t;
+    println!("{}",ans);
 }
--- abc176/src/bin/b.rs
+++ abc176/src/bin/b.rs
@@ -1,9 +1,22 @@
 use proconio::*;
+use proconio::marker::{Bytes, Chars};
 use std::*;
 
 const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        s : Chars
+    }
+    let mut ans = 0;
+    for ind in 0..s.len() {
+        ans += s[ind] as i32 - 48;
+    }
+    let ansStr = if ans % 9 == 0 {
+        "Yes"
+    } else {
+        "No"
+    };
+    println!("{}",ansStr);
 }
