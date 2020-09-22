2bdd9354b88b782909d0f24978d26bc5580288db
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-22 19:59:03 +0900

    3

--- abc178/src/bin/c.rs
+++ abc178/src/bin/c.rs
@@ -1,3 +1,20 @@
+use proconio::*;
+use std::*;
+
+const MOD: i128 = 100_000_007;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n : i128
+    }
+    if n <= 1 {
+        println!{"{}",0};
+    } else if n == 2 {
+        println!{"{}",2};
+    } else {
+        let mut ans = 1;
+        ans = ans * (n-2) * 9 * 2 % MOD;
+        ans = ans * (n * (n-1) % MOD / 2) % MOD;
+        println!{"{}",ans%MOD};
+    }
 }
---
+++ abc178/target/debug/c.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/81906/competitive_program_Rust/abc178/target/debug/c: /mnt/c/Users/81906/competitive_program_Rust/abc178/src/bin/c.rs build.rs
