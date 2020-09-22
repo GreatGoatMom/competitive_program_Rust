aecd6eaa8e78018965095081515f0ac8d3fc34df
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-22 21:41:48 +0900

    5

--- abc179/src/bin/a.rs
+++ abc179/src/bin/a.rs
@@ -5,5 +5,13 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input! {
+        s : String,
+    }
+    let slen = s.len();
+    if &s[slen-1 .. slen] == "s" {
+        println!("{}es",s);
+    } else {
+        println!("{}s",s);
+    }
 }
---
+++ target/debug/deps/a-1c3dbc0eec2f5256.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/a-1c3dbc0eec2f5256: src/bin/a.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/a-1c3dbc0eec2f5256.d: src/bin/a.rs
+
+src/bin/a.rs:
