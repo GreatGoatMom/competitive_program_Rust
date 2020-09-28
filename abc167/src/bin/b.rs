029689e88b6ca48cfe95c65811b8c5d181877028
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-29 01:27:47 +0900

    4

--- abc167/src/bin/b.rs
+++ abc167/src/bin/b.rs
@@ -8,5 +8,18 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        a : i64,
+        b : i64,
+        c : i64,
+        k : i64
+    }
+    let ans = if a >= k {
+        k
+    } else if a + b >= k {
+        a
+    } else {
+        a - (k - a - b)
+    };
+    println!{"{}",ans};
 }
---
+++ target/debug/deps/b-32318969adf840bc.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-32318969adf840bc: src/bin/b.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-32318969adf840bc.d: src/bin/b.rs
+
+src/bin/b.rs:
