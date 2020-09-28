4408cc6e04ca908684b75aa02d85449474465825
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-29 01:08:58 +0900

    2

--- abc168/src/bin/b.rs
+++ abc168/src/bin/b.rs
@@ -8,5 +8,14 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        n :usize,
+        s : Chars
+    }
+    let lens = s.len();
+    if lens > n {
+        println!("{}...",s.iter().take(n).collect::<String>());
+    } else {
+        println!("{}",s.iter().collect::<String>());
+    }
 }
---
+++ target/debug/deps/b-d6a955936f1c3c84.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-d6a955936f1c3c84: src/bin/b.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-d6a955936f1c3c84.d: src/bin/b.rs
+
+src/bin/b.rs:
