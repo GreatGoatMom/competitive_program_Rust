7154996e03695ff0ec3cf1d8fa6615b4a5437c01
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-09-30 00:37:11 +0900

    make abc166

---
+++ target/release/deps/a-8c2589eeb9f2db54.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/release/deps/a-8c2589eeb9f2db54: src/bin/a.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/release/deps/a-8c2589eeb9f2db54.d: src/bin/a.rs
+
+src/bin/a.rs:
