7a334d6d9e408da515b89fa25da3eb37ca056575
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-02 11:56:10 +0900

    2

--- abc165/src/bin/b.rs
+++ abc165/src/bin/b.rs
@@ -8,5 +8,17 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        a : i128
+    }
+    let mut ans = 0;
+    let mut sum_num = 100;
+    while sum_num < a {
+        ans += 1;
+        sum_num += sum_num / 100;
+    }
+    println!{
+        "{}",
+        ans
+    };
 }
---
+++ target/debug/deps/b-e7e441c7d80c5757.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-e7e441c7d80c5757: src/bin/b.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/b-e7e441c7d80c5757.d: src/bin/b.rs
+
+src/bin/b.rs:
