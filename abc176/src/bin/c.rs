9c49188f862f3deffa847b1da434e116d744e624
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-03 01:21:44 +0900

    2

--- abc176/src/bin/c.rs
+++ abc176/src/bin/c.rs
@@ -5,5 +5,19 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        n : usize,
+        n_list: [i64;n]
+    }
+    let mut ans = 0;
+    let mut high = n_list[0];
+    for i in 1..n {
+        if high > n_list[i] {
+            ans += high - n_list[i];
+        } else {
+            high = n_list[i];
+        }
+    }
+    println!{"{}",ans};
+
 }
---
+++ target/debug/deps/c-d739577cbcf8b146.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-d739577cbcf8b146: src/bin/c.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-d739577cbcf8b146.d: src/bin/c.rs
+
+src/bin/c.rs:
