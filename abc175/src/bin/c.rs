af043756b4d565694aa32ab6372b23b535e31c55
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-03 10:54:22 +0900

    1

--- abc175/src/bin/c.rs
+++ abc175/src/bin/c.rs
@@ -5,5 +5,22 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        mut x : i128,
+        k : i128,
+        d : i128
+    }
+    x = x.abs();
+    let mut ans = 0;
+    if x - k * d > 0 {
+        ans = x - k * d;
+    } else {
+        let just_before = x / d;
+        if (k - just_before) % 2 == 0 {
+            ans = x % d;
+        } else {
+            ans = (x - (d * just_before)) - d;
+        }
+    }
+    println!("{}",ans.abs());
 }
---
+++ target/debug/deps/c-93576d8d3e714fb0.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-93576d8d3e714fb0: src/bin/c.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-93576d8d3e714fb0.d: src/bin/c.rs
+
+src/bin/c.rs:
