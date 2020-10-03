ecedd4368f091aa8e3fc14ad0f37ddc126d76ff3
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-03 17:19:39 +0900

    6

--- abc171/src/bin/c.rs
+++ abc171/src/bin/c.rs
@@ -7,5 +7,15 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        mut n : usize
+    }
+    let mut ans = Vec::new();
+    while n != 0 {
+        n-=1;
+        ans.push(('a' as u8 + (n%26) as u8) as char);
+        n/=26;
+    }
+    ans.reverse();
+    println!("{}",ans.iter().collect::<String>());
 }
---
+++ target/debug/deps/c-977e9696368d22c6.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-977e9696368d22c6: src/bin/c.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-977e9696368d22c6.d: src/bin/c.rs
+
+src/bin/c.rs:
