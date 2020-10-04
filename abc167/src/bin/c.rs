1e196db5893d28d7bc25a2e9af187a415b9521e4
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-04 20:10:01 +0900

    8

--- abc167/src/bin/c.rs
+++ abc167/src/bin/c.rs
@@ -8,5 +8,37 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        n : usize,
+        m : usize,
+        x : usize,
+        books : [[usize;m+1]; n]
+    }
+    let mut ans = 1_000_000_000;
+    for bit in 0..=(1<<n) {
+        let mut tmp_cnt = 0;
+        let mut scores = vec![0;m];
+        for i in 0..n {
+            if (bit & 1<<i) != 0 {
+                for point in 1..=m {
+                    scores[point-1] += books[i][point];
+                }
+                tmp_cnt += books[i][0]
+            }
+        }
+        let mut is_reach = true;
+        for s in 0..m {
+            if scores[s] < x {
+                is_reach = false;
+            }
+        }
+        if is_reach {
+            ans = cmp::min(ans,tmp_cnt);
+        }
+    }
+    if ans == 1_000_000_000 {
+        println!("-1");
+        return;
+    }
+    println!("{}",ans);
 }
---
+++ target/debug/deps/c-723f88a4ebc71e1d.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-723f88a4ebc71e1d: src/bin/c.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-723f88a4ebc71e1d.d: src/bin/c.rs
+
+src/bin/c.rs:
