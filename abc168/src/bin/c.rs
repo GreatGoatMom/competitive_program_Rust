aba6eb58045cd34cace3beeb2c3d1536573a3a99
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-04 19:50:45 +0900

    5

--- abc168/src/bin/c.rs
+++ abc168/src/bin/c.rs
@@ -8,5 +8,22 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        a : f64,
+        b : f64,
+        h : usize,
+        m : usize
+    }
+    let h_deg = (h * 30) as f64 + 0.5 * m as f64;
+    let m_deg = (m * 6) as f64;
+    let mut deg = if h_deg > m_deg {
+        h_deg - m_deg
+    } else {
+        m_deg - h_deg
+    };
+    if deg > 180. {
+        deg = 360. - deg;
+    }
+    let ans = a * a + b * b - 2. * a * b * ((deg as f64).to_radians()).cos();
+    println!("{}",ans.sqrt());
 }
---
+++ target/debug/deps/c-e3a1a0ad86166d59.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-e3a1a0ad86166d59: src/bin/c.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-e3a1a0ad86166d59.d: src/bin/c.rs
+
+src/bin/c.rs:
