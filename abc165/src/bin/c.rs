a55badd525db2e8fc5b82738c3ced22fe571d391
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-06 18:49:44 +0900

    1

--- abc165/src/bin/c.rs
+++ abc165/src/bin/c.rs
@@ -6,7 +6,48 @@ use std::*;
 #[allow(dead_code)]
 const MOD: i128 = 100_000_007;
 
-#[fastout]
+fn score(vec: &Vec<usize>,list: &Vec<Vec<usize>>,q :usize) -> usize{
+    let mut res = 0;
+    for ind in 0..q {
+        if vec[list[ind][1]] - vec[list[ind][0]] == list[ind][2] {
+            res += list[ind][3];
+        }
+    }
+    res
+}
+
+fn dfs(vec : &mut Vec<usize>,n : usize,m : usize,list : &Vec<Vec<usize>>, q : usize) -> usize{
+
+    if vec.len() == n {
+        return score(vec,list,q);
+    }
+
+    let pre_num = match vec.last() {
+        Some(i) => *i,
+        None => 1
+    };
+    let mut res = 0;
+    for i in pre_num..=m {
+        vec.push(i);
+        res = cmp::max(res,dfs(vec,n,m,list,q));
+        vec.pop();
+    }
+    res
+}
+
+
 fn main() {
-    unimplemented!();
+    input!{
+        n : usize,
+        m : usize, 
+        q : usize, 
+        mut list : [[usize;4];q]
+    }
+    for l_ind in 0..q {
+        list[l_ind][0] -= 1;
+        list[l_ind][1] -= 1;
+    }
+    let mut origin_list = vec![];
+    let ans = dfs(&mut origin_list,n,m,&list,q);
+    println!("{}",ans);
 }
---
+++ target/debug/deps/c-830d9ae21262b370.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-830d9ae21262b370: src/bin/c.rs
+
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/deps/c-830d9ae21262b370.d: src/bin/c.rs
+
+src/bin/c.rs:
