70b306269222f0a270d440d372fa19d86eea0195
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-10 18:41:24 +0900

    1

--- abc178/src/bin/a.rs
+++ abc178/src/bin/a.rs
@@ -2,14 +2,9 @@ use proconio::*;
 
 #[fastout]
 fn main() {
-    input! {
-        n : u32,
+    input!{
+        n : isize
     }
-    let ans;
-    if n == 1 {
-        ans = 0;
-    } else {
-        ans = 1;
-    }
-    println!{"{}", ans};
+    let mut ans = n - 1;
+    println!("{}",ans.abs());
 }
--- abc179/src/bin/d.rs
+++ abc179/src/bin/d.rs
@@ -8,28 +8,9 @@ fn main() {
     input!{
         n : usize,
         k : usize
+        list : [[usize;2];k]
     }
-    let mut num_list = [0;20_0002];
-    for _ in 0..k {
-        input!{
-            a : usize,
-            b : usize
-        }
-        num_list[a] += 1;
-        num_list[b+1] -= 1; 
-    }
-    for ind in 1..=200_000 {
-        num_list[ind] += num_list[ind-1];
-    }
-    let mut dp = [0;20_0001];
-    dp[1] = 1;
-    for i in 0..20_000 {
-        for j in &num_list {
-            if i-j >= 0 {
-                dp[i-j] += dp[i];
-            }
-        }
-    }
-    let ans = dp[n+1] % 998244353;
-    println!("{}",ans);
+    let mut dp = vec![0;200001];
+    let mut s_dp = vec![0;200001];
+    
 }
