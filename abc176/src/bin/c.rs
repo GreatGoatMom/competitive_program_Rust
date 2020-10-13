dfa433a19a6964084d1046a525cf740cd83ec24b
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-13 23:58:29 +0900

    5

--- abc176/src/bin/c.rs
+++ abc176/src/bin/c.rs
@@ -7,17 +7,15 @@ const MOD: i128 = 100_000_007;
 fn main() {
     input!{
         n : usize,
-        n_list: [i64;n]
+        mut list : [usize;n]
     }
     let mut ans = 0;
-    let mut high = n_list[0];
     for i in 1..n {
-        if high > n_list[i] {
-            ans += high - n_list[i];
-        } else {
-            high = n_list[i];
+        if list[i-1] > list[i] {
+            let dif = list[i-1] - list[i];
+            ans += dif;
+            list[i] += dif;
         }
     }
-    println!{"{}",ans};
-
+    println!("{}",ans);
 }
