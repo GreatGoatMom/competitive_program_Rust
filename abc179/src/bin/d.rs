ecb16236abf514d6d6eae57d0e2f5f25020a6384
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-21 23:15:07 +0900

    3

--- abc179/src/bin/d.rs
+++ abc179/src/bin/d.rs
@@ -1,8 +1,7 @@
 use proconio::*;
 use std::*;
 
-const MOD: i128 = 998244353;
-
+const MOD : usize = 998244353;
 fn main() {
     input!{
         n : usize,
@@ -10,25 +9,25 @@ fn main() {
         list : [[usize;2];k]
     }
     let mut dp = vec![0;n+1];
-    let mut s_dp = vec![0;n+1];
+    let mut sum_dp = vec![0;n+1];
     dp[0] = 1;
-    s_dp[1] = 1;
+    sum_dp[1] = 1;
     for i in 1..n {
-        for kind in 0..k {
-            let left = if i <= list[kind][1]{
+        for ik in 0..k {
+            let right = if i + 1< list[ik][0] {
                 0
             } else {
-                i - list[kind][1]
+                i + 1 - list[ik][0]
             };
-            let right =  if i + 1 <= list[kind][0] {
+            let left = if i < list[ik][1] {
                 0
             } else {
-                i - list[kind][0] + 1
-            };
-            dp[i] += (s_dp[right] - s_dp[left] + MOD) % MOD;
+                i - list[ik][1]
+            }; 
+            dp[i] += (sum_dp[right]  + MOD - sum_dp[left]) % MOD;
+            dp[i] %= MOD;
         }
-        s_dp[i+1] += s_dp[i]+dp[i];
-        s_dp[i+1] %= MOD;
+        sum_dp[i+1] += (sum_dp[i] + dp[i]) %MOD;
     }
-    println!("{}",dp[n-1]%MOD);
+    println!{"{}",dp[n-1]%MOD};
 }
