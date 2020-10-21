a856b30e50569a8707f8d6c5121790b3abd11ca3
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-22 00:12:44 +0900

    4

--- abc178/src/bin/d.rs
+++ abc178/src/bin/d.rs
@@ -1,3 +1,23 @@
+use proconio::*;
+use std::*;
+
+
+const MOD : usize = 1_000_000_007;
+
 fn main() {
-    unimplemented!();
+    input!{
+        s : usize
+    }
+    let mut dp = vec![0;s+1];
+    dp[0] = 1;
+    for i in 1..=s{
+        if i < 3 {
+            continue;
+        }
+        for j in 0..=i-3 {
+            dp[i] += dp[j];
+            dp[i] %= MOD;
+        }
+    }
+    println!{"{}",dp[s]};
 }
