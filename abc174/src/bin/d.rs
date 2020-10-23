1b5e652e139c549893ccbc1a2086acc906bef18a
YuukiInaba <tokumaru.program.yuuki@gmail.com>
2020-10-23 22:40:17 +0900

    2

--- abc174/src/bin/d.rs
+++ abc174/src/bin/d.rs
@@ -7,5 +7,27 @@ const MOD: i128 = 100_000_007;
 
 #[fastout]
 fn main() {
-    unimplemented!();
+    input!{
+        a : usize,
+        list : Chars
+    }
+    let mut ans = 0;
+    let mut left_white = 0;
+    let mut right_red = 0;
+    for i in 0..a {
+        if list[i] == 'R'{
+            right_red += 1;
+            ans += 1;
+        }
+    }
+    for i in 0..a {
+        if list[i] == 'W' {
+            left_white += 1;
+        } else {
+            right_red -= 1;
+        }
+        ans = cmp::min(ans,cmp::max(left_white,right_red));
+    }
+
+    println!{"{}",ans};
 }
--- target/debug/d.d
+++ target/debug/d.d
@@ -1,1 +1,1 @@
-/mnt/c/Users/81906/competitive_program_Rust/target/debug/d: /mnt/c/Users/81906/competitive_program_Rust/abc176/src/bin/d.rs build.rs
+/mnt/c/Users/81906/competitive_program_Rust/target/debug/d: /mnt/c/Users/81906/competitive_program_Rust/abc174/src/bin/d.rs build.rs
